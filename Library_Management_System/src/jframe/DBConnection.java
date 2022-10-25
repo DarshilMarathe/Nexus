b4a0763f86466304c9093501fdc74002aa1d5b85
Darshil Marathe <99969644+DarshilMarathe@users.noreply.github.com>
2022-10-25 22:12:03 +0530

    DB Connected successfully for signup

--- Library_Management_System/src/jframe/DBConnection.java
+++ Library_Management_System/src/jframe/DBConnection.java
@@ -17,7 +17,11 @@ public class DBConnection {
     public static Connection getConnection(){
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/library_ms","root","");
+//            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/library_ms","root","");
+            
+            
+//            Connection con = DBConnection.getConnection();
+             con = DriverManager.getConnection("jdbc:mysql://localhost:8111/library_ms","root","");
         }catch(Exception e){
             e.printStackTrace();
         }
